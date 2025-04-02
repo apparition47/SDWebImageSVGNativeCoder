0e054aff5764c52e9d2a54a14c8d78b148c37d53
Aaron Lee <aaron@onefatgiraffe.com>
2025-04-02 16:05:48 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-        .package(url: "https://github.com/apparition47/svgnative-Xcode.git", .branch("main")
+        .package(url: "https://github.com/apparition47/svgnative-Xcode.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
